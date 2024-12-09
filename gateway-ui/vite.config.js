42f2ac47b8659d378b7ece1508fd808211d2cef1
Liu Rui <lovearuis@gmail.com>
2024-12-09 18:53:53 +0800

    build-gatewayui-配置编译输出路径到后单资源目录

--- gateway-ui/vite.config.js
+++ gateway-ui/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../src/main/resources/META-INF/resources/', // 替换为你想要的输出路径
+    assetsDir: 'static', // 指定静态资源存放的文件夹
+    sourcemap: true, // 是否生成 sourcemap 文件
+  },
 })
