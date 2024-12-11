7e45bca697411a4c5d7a806d4fbc59fa10714388
Liu Rui <lovearuis@gmail.com>
2024-12-11 11:43:04 +0800

    feat-gatewayui-基本设置初步实现

---
+++ gateway-ui/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>仪表盘</h1>
+            <p>这里是仪表盘页面的内容。</p>
+        </div>
+    );
+};
+
+export default Dashboard;
